dc4f8fd0d9583254919065052d0252ee1196a863
Henry Kam <h3nrykam@gmail.com>
2021-10-27 01:08:55 -0700

    sync sequelize models to database

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,12 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
+  const productData = await Product.findAll();
   // be sure to include its associated Category and Tag data
+  // TO DO
+  return res.json(productData);
 });
 
 // get one product
